e1a49dd6871470917d2a45aec74d402c2685cbf2
Vadim <vadimgatsura@gmail.com>
2016-08-13 16:54:02 +0300

    Complete first task

---
+++ Advanced XML/AdvancedXML.Tests/XmlValidatorTests.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using NUnit.Framework;
+
+namespace AdvancedXML.Tests
+{
+    [TestFixture]
+    public class XmlValidatorTests
+    {
+        public static IEnumerable<TestCaseData> ValidateCaseSource
+        {
+            get
+            {
+                yield return new TestCaseData("http://library.by/catalog", "../book.xsd", "./InvalidDate.xml", false);
+
+                yield return new TestCaseData("http://library.by/catalog", "../book.xsd", "./InvalidGenre.xml", false);
+
+                yield return new TestCaseData("http://library.by/catalog", "../book.xsd", "./InvalidId.xml", false);
+
+                yield return new TestCaseData("http://library.by/catalog", "../book.xsd", "./InvalidISBN.xml", false);
+
+                yield return new TestCaseData("http://library.by/catalog", "../book.xsd", "../books.xml", true);
+            }
+        }
+
+        [OneTimeSetUp]
+        public void Initialize()
+        {
+            Directory.SetCurrentDirectory(@"D:\Projects\.Net-Mentoring-D2-D3\Advanced XML\AdvancedXML.Tests");
+        }
+
+        [Test, TestCaseSource(nameof(ValidateCaseSource))]
+        public void ValidateTests(string targetNamespace, string schemaPath, string filePath, bool expectedResult)
+        {
+            // Arrange
+            Console.WriteLine(Directory.GetCurrentDirectory());
+            var validator = new XmlValidator();
+            IList<string> errors;
+
+            // Act
+            var result = validator.Validate(targetNamespace, schemaPath, filePath, out errors);
+
+            // Assert
+            Assert.AreEqual(expectedResult, result);
+            Assert.AreEqual(result, !errors.Any());
+
+            Console.WriteLine($"Errors: {string.Join(";", errors)}");
+        }
+    }
+}
---
+++ Advanced XML/AdvancedXML/XmlValidator.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Xml;
+using System.Xml.Linq;
+using System.Xml.Schema;
+
+namespace AdvancedXML
+{
+    public class XmlValidator
+    {
+        /// <summary>
+        /// Validate xml file with xsd schema
+        /// </summary>
+        /// <param name="targetNamespace">The schema targetNamespace property, or null to use the targetNamespace specified in the schema.</param>
+        /// <param name="schemaPath">The path to file that specifies the schema to load.</param>
+        /// <param name="filePath">The path to file that specifies xml document to load</param>
+        /// <param name="errors">The list of errors during validation of xml document</param>
+        /// <returns>Validation result. If xml file has errors - false, otherwise - true</returns>
+        public bool Validate(string targetNamespace, string schemaPath, string filePath, out IList<string> errors)
+        {
+            if (string.IsNullOrWhiteSpace(schemaPath))
+                throw new ArgumentException(nameof(schemaPath));
+
+            if(string.IsNullOrWhiteSpace(filePath))
+                throw new ArgumentException(nameof(filePath));
+
+            errors = new List<string>();
+
+            var schemas = new XmlSchemaSet();
+            schemas.Add(targetNamespace, XmlReader.Create(new FileStream(schemaPath, FileMode.Open, FileAccess.Read)));
+
+            var document = XDocument.Load(new FileStream(filePath, FileMode.Open));
+
+            var list = errors;
+            document.Validate(schemas, (o, e) =>
+            {
+                list.Add(e.Message);
+            });
+
+            return !errors.Any();
+        }
+    }
+}
