6b91e6ccab4d787ca0f989c489f9c91cdd03b9bd
Vadim <vadimgatsura@gmail.com>
2016-09-13 16:16:06 +0300

    Initial commit

---
+++ Windows Services/PhotoProcessingService/PhotoProcessingService.cs
@@ -0,0 +1,25 @@
+﻿using Topshelf;
+using Topshelf.Logging;
+
+namespace PhotoProcessingService
+{
+    public sealed class PhotoProcessingService : ServiceControl
+    {
+        private readonly LogWriter logWriter;
+
+        public PhotoProcessingService()
+        {
+            logWriter = HostLogger.Get<PhotoProcessingService>();
+        }
+
+        public bool Start(HostControl hostControl)
+        {
+            return true;
+        }
+
+        public bool Stop(HostControl hostControl)
+        {
+            return true;
+        }
+    }
+}
---
+++ Windows Services/PhotoProcessingService/Program.cs
@@ -0,0 +1,22 @@
+﻿using Topshelf;
+
+namespace PhotoProcessingService
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HostFactory.New(x => x.UseNLog());
+
+            HostFactory.Run(x =>
+            {
+                x.Service<PhotoProcessingService>();
+
+                x.SetServiceName("Photo Processing Service");
+                x.SetDisplayName("Photo Service");
+                x.SetDescription("Photo Service");
+                x.RunAsLocalSystem();
+            });
+        }
+    }
+}
